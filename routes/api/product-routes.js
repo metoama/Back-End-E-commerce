88702b00628b6e6d8fba75e4f3c2c17145e39a9a
CAGE1899 <metoama@gmail.com>
2023-02-16 03:12:23 -0500

    api routes for get all and one complete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,37 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try{
+    const productDate = await Product.findAll({
+      include: [Category,Tag],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json({ message: 'error'});
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [Category, Tag],
+    });
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({message: "Product not found"});
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'error'})
+  }
+  
 });
 
 // create new product
