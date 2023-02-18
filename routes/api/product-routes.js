16a5d3003e272c0b14eff6d696b31f26a8146303
CAGE1899 <metoama@gmail.com>
2023-02-17 19:47:17 -0500

    completed product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -73,7 +73,7 @@ router.post('/', async (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id',  (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -114,8 +114,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: { id: req.params.id},
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product with this id found' });
+      return;
+    }
+
+    res.status(200).json({ message: `Product: ${req.params.id} was deleted!` });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
